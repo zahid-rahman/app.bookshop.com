d6f67aa5490879338ccc13712b9010f93b8ced50
zahid-rahman <rahmanzahid94@gmail.com>
2023-07-10 02:22:58 +0600

    feat: add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { FormsModule } from "@angular/forms";
 import { BookComponent } from './books/book/book.component';
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
     declarations: [AppComponent, CartComponent],
-    imports: [BrowserModule, FormsModule, BooksModule],
+    imports: [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
